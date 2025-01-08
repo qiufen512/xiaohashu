b6a628b6d3fa3e60a56cf3171e699ef026a48c1b
PingChangXin404 <645210207@qq.com>
2025-01-08 21:12:19 +0800

    完善了小哈书的骨架工程，添加了framework模块，在该模块中添加了xiaoha-common模块，后续如一些业务上通用的枚举、工具类等等，都可以放置此模块中。最后，我们在认证服务的 pom.xml 中，引入了 common 模块，并创建了一个测试接口，通过 Response 工具类，对前端成功返回了响应参数。

---
+++ xiaoha-framework/xiaoha-common/src/main/java/com/qiufen/framewok/common/exception/BaseExceptionInterface.java
@@ -0,0 +1,10 @@
+package com.qiufen.framewok.common.exception;
+
+public interface BaseExceptionInterface {
+
+    // 获取异常码
+    String getErrorCode();
+
+    // 获取异常信息
+    String getErrorMessage();
+}
