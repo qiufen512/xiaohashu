36aa9b922defa77e32bb59af0521e9fa1d7fcd16
PingChangXin404 <645210207@qq.com>
2025-01-12 16:32:37 +0800

    自定义jackson-starter支持LocalDateTime日期API，整合Logback日志框架（支持异步写入），整合flatten-maven-plugin插件，解决子模块单独打包失败问题

--- xiaoha-framework/xiaoha-common/src/main/java/com/qiufen/framewok/common/util/JsonUtils.java
+++ xiaoha-framework/xiaoha-common/src/main/java/com/qiufen/framewok/common/util/JsonUtils.java
@@ -11,26 +11,35 @@ import lombok.SneakyThrows;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
 public class JsonUtils {
 
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     // todo
     static {
         OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        // JavaTimeModule 用于指定序列化和反序列化规则
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-        // 支持 LocalDateTime
-        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DateConstants.Y_M_D_H_M_S_FORMAT)));
-        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DateConstants.Y_M_D_H_M_S_FORMAT)));
+//        // JavaTimeModule 用于指定序列化和反序列化规则
+//        JavaTimeModule javaTimeModule = new JavaTimeModule();
+//        // 支持 LocalDateTime
+//        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DateConstants.Y_M_D_H_M_S_FORMAT)));
+//        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DateConstants.Y_M_D_H_M_S_FORMAT)));
         // 解决 LocalDateTime 的序列化问题
-        OBJECT_MAPPER.registerModules(javaTimeModule);
+        OBJECT_MAPPER.registerModules(new JavaTimeModule());
     }
 
     /**
-     *  将对象转换为 JSON 字符串
+     * 由于自定义了jackson配置类，与此处的代码重复，故在此处创建init方法，由
+     * jackson配置类调用覆盖此处的对象
+     * @param objectMapper
+     */
+    public static void init(ObjectMapper objectMapper) {
+        OBJECT_MAPPER = objectMapper;
+    }
+
+    /**
+     * 将对象转换为 JSON 字符串
+     *
      * @param obj
      * @return
      */
